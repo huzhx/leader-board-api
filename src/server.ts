d2f16da86bdc23e94d0c0f733c25d7c731cf2c57
Zhaoxian Hu <huzhx@uci.edu>
2021-05-30 13:46:18 -0700

    config the datasources

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,30 @@ import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
 import { createServer } from 'http';
 import cors from 'cors';
+import 'dotenv/config';
 import typeDefs from './graphql/schema';
 import resolvers from './graphql/resolvers';
+import LeaderboardSQLDb from './datasources/leaderboard-sql-db';
 
 const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(cors());
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const knexConfig = {
+  client: 'pg',
+  connection: {
+    host: process.env.PG_HOST!,
+    user: process.env.PG_USER!,
+    password: process.env.PG_PASSWORD!,
+    database: process.env.PG_DB!,
+  },
+};
+
+const dataSources = () => ({
+  leaderboardSQLDb: new LeaderboardSQLDb(knexConfig),
+});
+
+const server = new ApolloServer({ typeDefs, resolvers, dataSources });
 
 server.applyMiddleware({ app, path: '/graphql' });
 const httpServer = createServer(app);
